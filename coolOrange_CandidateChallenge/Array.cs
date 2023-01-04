2e966ea509c88469cde71d63a8d7dd1f0ea18e7d
julian192 <118811870+julian192@users.noreply.github.com>
2023-01-04 11:03:14 +0100

    04.01.2023

--- coolOrange_CandidateChallenge/Array.cs
+++ coolOrange_CandidateChallenge/Array.cs
@@ -13,7 +13,11 @@ namespace coolOrange_CandidateChallenge
          */
         public static int FindMaxValue(int[] array, int position1, int position2)
         {
-            return -1;
+            int max = array[position1];
+            for(int i = position1; i < position2; i++)
+                if (array[i] > max)
+                    max = array[i];
+            return max;
         }
 
         /**
@@ -21,7 +25,15 @@ namespace coolOrange_CandidateChallenge
          */
         public static int FindMinPosition(int[] array, int position1, int position2)
         {
-            return -1;
+            int min = array[position1];
+            int minposition = position1;
+            for(int i = position1; i < position2; i++)
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    minposition = i;
+                }
+            return minposition;
         }
 
         /**
@@ -29,6 +41,9 @@ namespace coolOrange_CandidateChallenge
          */
         public static void Swap(int[] array, int position1, int position2)
         {
+            int tmp = array[position1];
+            array[position1] = array[position2];
+            array[position2] = tmp;
         }
 
         /**
@@ -36,6 +51,12 @@ namespace coolOrange_CandidateChallenge
          */
         public static void ShiftLeftByOne(int[] array, int position1, int position2)
         {
+            int tmp = array[position1];
+            for(int i = position1; i < position2 - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            array[position2 - 1] = tmp;
         }
 
         /**
@@ -43,7 +64,11 @@ namespace coolOrange_CandidateChallenge
          */
         public static int[] CreateRandomArray(int size, int minValue, int maxValue)
         {
-            return new int[size];
+            var rand = new Random();
+            int[] ret = new int[size];
+            for (int i = 0; i < size; i++)
+                ret[i] = rand.Next(minValue, maxValue);
+            return ret;
         }
 
         /**
@@ -51,7 +76,12 @@ namespace coolOrange_CandidateChallenge
          */
         public static int[,] CreateRandomMatrix(int rows, int columns, int minValue, int maxValue)
         {
-            return new int[rows, columns];
+            var rand = new Random();
+            int[,] ret = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    ret[i, j] = rand.Next(minValue, maxValue);
+            return ret;
         }
 
         /**
@@ -59,17 +89,32 @@ namespace coolOrange_CandidateChallenge
          */
         public static int[,] CopyArray(int[] array)
         {
-            return  new int[array.Length,array.Length];
+            int[,] ret = new int[array.Length, 2];
+            for(int i = 0; i < array.Length; i++)
+                ret[i, 0] = ret[i, 1] = array[i];
+            return ret;
         }
 
         /**
          *  returns a random position of the passed number in the sorted array. The function returns −1 if the number is not present in the array. 
-         * The method assumes that the array is already sorted. 
-         * Bonus: Exploit the fact that the array is sorted to find an efficient algorithm.
-         * (Hint: Binary search algorithm)
+         *  The method assumes that the array is already sorted. 
+         *  Bonus: Exploit the fact that the array is sorted to find an efficient algorithm.
+         *  (Hint: Binary search algorithm)
          */
         public static int FindInSortedArrary(int[] array, int number)
         {
+            int start = 0;
+            int end = array.Length - 1;
+            while(start <= end)
+            {
+                int middle = start + (end - start) / 2;
+                if(number < array[middle])
+                    end = middle - 1;
+                else if(number > array[middle])
+                    start= middle + 1;
+                else
+                    return middle;
+            }
             return -1;
         }
     }
